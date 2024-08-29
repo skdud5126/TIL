a10408dc9ec752ecd025f99901d6b9134ebeba3a
skdud5126 <skdud5126@gmail.com>
2024-08-29 17:57:54 +0900

    up3 24.08.29

--- algo/start/code/conversion.py
+++ algo/start/code/conversion.py
@@ -2,21 +2,30 @@
 
 def conversion(num, decimal ):
     res = []
-    while num!=0:
-        res.append(num%decimal)
-        num//=decimal
-    res.reverse()
-    res_fin.append(''.join(map(str,res)))
+    hex_num = '0123456789ABCDEF'
+    if decimal == 16:
+        while num!=0:
+            res.append(hex_num[num%decimal])
+            num//=decimal
+        res.reverse()
+        res_fin.append(''.join(res))
+    else:
+        while num!=0:
+            res.append(num%decimal)
+            num//=decimal
+        res.reverse()
+        res_fin.append(''.join(map(str,res)))
     return res_fin
+
+
 
 num = int(input())
 
 li = [2, 8, 16]  # 진수 목록
 res_fin = []  # 2, 8, 16진수 나타낼 목록
+
 for i in range(len(li)):
     conversion(num, li[i])
-
-# print(*res_fin)
 
 for de in range(len(res_fin)):
     print(f'{li[de]}진수 : {res_fin[de]}', end =' / ')
