6edde41ac5982174479c9e25a687299eb1a073d6
skdud5126 <skdud5126@gmail.com>
2024-09-04 17:59:43 +0900

    up0 24.09.04

--- algo/APS/분할정복/code/2.퀵_정렬.py
+++ algo/APS/분할정복/code/2.퀵_정렬.py
@@ -12,13 +12,13 @@ def hoare_partition1(left, right):
     j = right
 
     while i <= j:
-        while i <= j and arr[i] <= pivot:
+        while i <= j and arr[i] <= pivot: # i는 pivot보다 작거나 같으면 +1 : 큰 값 탐색
             i += 1
 
-        while i <= j and arr[j] >= pivot:
+        while i <= j and arr[j] >= pivot: # j는 pivot보다 크거나 같으면 -1 : 작은값 탐색
             j -= 1
 
-        if i < j:
+        if i < j:  # 스왑
             arr[i], arr[j] = arr[j], arr[i]
 
     arr[left], arr[j] = arr[j], arr[left]
