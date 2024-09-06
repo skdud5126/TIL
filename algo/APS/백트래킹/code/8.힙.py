52ec836838f88b7cee6b2c7a9164c4b6d0e1ade0
skdud5126 <skdud5126@gmail.com>
2024-09-06 17:52:48 +0900

    up3 24.09.06

--- algo/APS/백트래킹/code/8.힙.py
+++ algo/APS/백트래킹/code/8.힙.py
@@ -1,26 +1,50 @@
-from heapq import heappush, heappop
+# from heapq import heappush, heappop
 
-arr = [20, 15, 19, 4, 13, 11]
+# arr = [20, 15, 19, 4, 13, 11]
 
-# 최소힙
-min_heap = []
+# # 최소힙
+# min_heap = []
 
-for el in arr:
-    heappush(min_heap, el)
+# for el in arr:
+#     heappush(min_heap, el)
 
-print(min_heap)  # [4, 13, 11, 20, 15, 19] 출력
+# print(min_heap)  # [4, 13, 11, 20, 15, 19] 출력
 
-while len(min_heap) > 0:
-    print(heappop(min_heap), end=' ')
+# while len(min_heap) > 0:
+#     print(heappop(min_heap), end=' ')
 
-print()
+# print()
 
-# 최대힙
-max_heap = []
-for el in arr:
-    heappush(max_heap, -el)
+# # 최대힙
+# max_heap = []
+# for el in arr:
+#     heappush(max_heap, -el)
 
-print(max_heap)  # [-20, -15, -19, -4, -13, -11] 출력
+# print(max_heap)  # [-20, -15, -19, -4, -13, -11] 출력
 
-while len(max_heap) > 0:
-    print(-heappop(max_heap), end=' ')
+# while len(max_heap) > 0:
+#     print(-heappop(max_heap), end=' ')
+
+def enq(n):
+    global last
+    last +=1
+    h[last] = n
+    c = last
+    p = c//2
+    while p>=1 and h[p] < h[c]:
+        h[p], h[c] = h[c], h[p]
+        c = p
+        p = c//2
+
+
+
+N = int(input())
+arr = list(map(int,input().split()))
+
+h = [0] * (N+1)
+last = 0
+
+for num in arr:
+    enq(num)
+
+print(h)
