32d110a5ad96a6b9f0d967d707f2267f89df45da
skdud5126 <skdud5126@gmail.com>
2024-09-24 10:33:20 +0900

    up2 24.09.24

--- Django/05_ORM_with_view/code/articles/views.py
+++ Django/05_ORM_with_view/code/articles/views.py
@@ -20,3 +20,31 @@ def detail(request, pk):
     }
 
     return render(request, 'articles/detail.html', context)
+
+def new(request):
+    # 게시글 작성 페이지 응답
+
+    return render(request, 'articles/new.html')
+
+def create(request):
+    # 1. 사용자 요청으로부터 입력 데이터를 추출
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # 저장1  (save전 유효성 검사가능 but 하나씩 지정해줘야함 불편.)
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    # 저장2
+
+    article = Article(title = title, content=content)
+    article.save()
+
+    # 저장3 (유효성 검사(보안검사)할 타이밍이 없음)
+    # Article.objects.create(title=title, content = content)
+
+    # 3. 추출한 입력 데이터를 활용해 DB에 저장 요청
+
+    return render(request, 'articles/create.html')
