fc1ded9d1af659eb071e492096221fc6b228e921
skdud5126 <skdud5126@gmail.com>
2024-09-29 03:39:13 +0900

    up2 24.09.29

--- Django/07_Static/code/crud/settings.py
+++ Django/07_Static/code/crud/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    # python 객체지향 경로 시스템
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
