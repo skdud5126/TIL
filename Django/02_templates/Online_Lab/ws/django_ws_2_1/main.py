c67408afa2752f917c7a61d896f82d1bdc888d1e
skdud5126 <skdud5126@gmail.com>
2024-09-30 17:22:24 +0900

    up4 24.09.30

---
+++ Django/02_templates/Online_Lab/ws/django_ws_2_1/main.py
@@ -0,0 +1,29 @@
+import requests
+from pprint import pprint
+
+API_URL = 'http://www.aladin.co.kr/ttb/api/ItemList.aspx'
+API_KEY = 'ttbskdud51261424002'
+params = {
+    'ttbkey': API_KEY,
+    'QueryType' : 'ItemNewSpecial',
+    'SearchTarget' : 'Book',
+    'Output' : 'js',
+    'Start' : 1,
+    'MaxResults' : 50,
+    'Version' : '20131101',
+}
+
+response = requests.get(API_URL, params= params).json()
+
+items = response.get('item')
+
+res = []
+dict_value = ['isbn','author', 'title', 'pubDate']
+dict_key = ['국제 표준 도서 번호', '저자', '제목', '출간일']
+for item in items:
+    dict = {}
+    for num in range(len(dict_value)):
+        dict[dict_key[num]] = item.get(dict_value[num])
+    res.append(dict)
+
+pprint(res)
