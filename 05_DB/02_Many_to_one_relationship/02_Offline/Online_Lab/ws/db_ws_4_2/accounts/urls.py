0c8a06c7a8b381d47fc69737b277e9fe4bda4caf
skdud5126 <skdud5126@gmail.com>
2024-10-23 17:51:54 +0900

    up9 24.10.23

---
+++ 05_DB/02_Many_to_one_relationship/02_Offline/Online_Lab/ws/db_ws_4_2/accounts/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+app_name = 'accounts'
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+]
