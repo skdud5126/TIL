aebadccd7960963eab49b91624ccd44d585d57fd
skdud5126 <skdud5126@gmail.com>
2024-11-10 03:04:21 +0900

    up1 24.11.10

---
+++ 07_Vue/04_Single_File_Component/code/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="greeting">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello')
+</script>
+
+<style scoped>
+.greeting {
+  color : red;
+}
+</style>
