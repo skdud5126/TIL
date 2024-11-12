ae44d7e68a11b3f5ac77ae37c3b7c0d182fff626
skdud5126 <skdud5126@gmail.com>
2024-11-12 17:56:25 +0900

    up2 24.11.12

---
+++ 07_Vue/06_Vue_Router/Online_Lab/hw/vue_hw_6_2/my-third-front-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MainPage />
+    <RouterLink :to="{name:'some'}">Some</RouterLink> | 
+    <RouterLink :to="{name : 'other'}">Other</RouterLink>
+
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import MainPage from '@/components/MainPage.vue';
+</script>
+
+<style scoped>
+
+</style>
