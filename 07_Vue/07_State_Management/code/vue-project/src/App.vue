3adbed9b0931df70748c1ff2ef57da6b1e2cf98d
skdud5126 <skdud5126@gmail.com>
2024-11-14 10:47:32 +0900

    up

--- 07_Vue/07_State_Management/code/vue-project/src/App.vue
+++ 07_Vue/07_State_Management/code/vue-project/src/App.vue
@@ -1,12 +1,55 @@
+<template>
+  <div>
+    <h1>Todo Project</h1>
+    <h2>완료된 Todo 개수 : {{ store.doneTodosCount }}</h2>
+    <TodoList />
+    <TodoForm />
+  </div>
+</template>
+
 <script setup>
+import TodoForm from '@/components/TodoForm.vue';
+import TodoList from '@/components/TodoList.vue';
+import { useCounterStore } from '@/stores/counter';
+
+const store = useCounterStore()
+
+
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+<!-- <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 // 1. 중앙 저장소 가져오기
 import { useCounterStore } from '@/stores/counter';
+
+// 2. 중앙 저장소를 활용해 인스턴스 생성
+const store = useCounterStore()
+
+// 3. 중앙 저장소에 상태를 참조
+console.log(store.count)
+
+// 액션 호출
+// store.increment()
+
+// [주의] 각 컴포넌트에서는 중앙저장소에 새로운 상태를 생성할 수 없다.
+// store.ssafy = 'ssafy'
+
+
 </script>
 
 <template>
   <header>
+    <div>
+      <p>getters: {{ store.doubleCount }}</p>
+      <button @click="store.increment()">++</button>
+    </div>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -46,4 +89,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
